8a977354b714305a62836dbed567f84c0980ae0e
Dinar <19yakudza99@gmail.com>
2025-05-20 18:24:03 +0500

    SelenideLogger.addListener was added

--- src/test/java/tests/JavaFakerTest.java
+++ src/test/java/tests/JavaFakerTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,7 @@ public class JavaFakerTest {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
     }
 
